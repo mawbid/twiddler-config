8823934d035b678f1f719e6d397f0a97bf8eadd6
Haukur Hreinsson <hawk@mawbid.com>
2024-04-26 03:10:26 +0000

    arena, argv

--- conv.cpp
+++ conv.cpp
@@ -1,5 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/mman.h>
 
 enum MouseClick
 {
@@ -61,19 +63,76 @@ struct Header
 
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
 
+struct Arena
+{
+    void *base;
+    void *next;
+    size_t size;
+};
+
+#define KiloBytes(n) (1024 * (n))
+#define MegaBytes(n) (1024 * KiloBytes((n)))
+#define GigaBytes(n) (1024 * MegaBytes((n)))
+
+#define MAIN_ARENA_BASE (void *)0x1000000
+
+inline void
+initArena(Arena *arena, size_t size, void *addressHint = 0)
+{
+    *arena = {};
+    void *base = mmap(addressHint, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    if (base == MAP_FAILED)
+    {
+        fprintf(stderr, "Could not allocate memory\n");
+        exit(1);
+    }
+    else
+    {
+        arena->base = base;
+        arena->next = base;
+        arena->size = size;
+    }
+}
+
+size_t bytesAvailable(Arena *arena)
+{
+    size_t used = (u8 *)(arena->next) - (u8 *)(arena->base);
+    return arena->size - used;
+}
+
+void takeBytes(Arena *arena, size_t bytes)
+{
+    arena->next = (void *)((u8 *)arena->next + bytes);
+}
+
 int main(int argc, char **argv)
 {
-    FILE *fp = fopen("twiddler.cfg", "r");
-    u8 configurationBuffer[1024 * 1024];
+    FILE *fp = fopen(argv[1], "r");
+    Arena arena;
+    initArena(&arena, MegaBytes(1), MAIN_ARENA_BASE);
 
     if (fp)
     {
-        u64 length = fread(configurationBuffer, sizeof(*configurationBuffer), ARRAY_SIZE(configurationBuffer), fp);
+        char *configurationBuffer = (char *)arena.next;
+
+        u64 length = fread(configurationBuffer, sizeof(*configurationBuffer), bytesAvailable(&arena), fp);
+        takeBytes(&arena, length);
+
         if (feof(fp))
         {
             Header *header = (Header *)configurationBuffer;
             printf("yay! %lu\n", length);
         }
+        else
+        {
+            fprintf(stderr, "Could not read file %s\n", argv[1]);
+            exit(1);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Could not open file %s\n", argv[1]);
+        exit(1);
     }
     printf("hello\n");
     return 0;
