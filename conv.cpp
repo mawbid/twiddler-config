018ad4a64fe752d1234b52a262e9e9a6e32c5b01
Haukur Hreinsson <hawk@mawbid.com>
2024-05-03 02:06:43 +0000

    Parse string tables

--- conv.cpp
+++ conv.cpp
@@ -30,11 +30,22 @@ struct Header
     u8 options_c;
 };
 
+struct HidPair
+{
+    u8 modifiers;
+    u8 code;
+};
+
 struct ChordTableEntry
 {
     u16 buttons;
-    u8 hidModifiers;
-    u8 hidCode;
+    HidPair hid;
+};
+
+struct StringContentsTableEntry
+{
+    u16 length;
+    HidPair elements[];
 };
 
 #pragma pack(pop)
@@ -55,6 +66,7 @@ struct Arena
 #define MAIN_ARENA_BASE (void *)0x1000000
 
 #define MAX_CHORD_COUNT 1020
+#define MAX_STRING_COUNT 1024
 
 inline void
 initArena(Arena *arena, size_t size, void *addressHint = 0)
@@ -143,10 +155,13 @@ struct TwiddlerConfig
         Success,
         IncompleteHeader,
         ChordCountTooHigh,
+        TooManyStrings,
+        StringHasInconsistentLength, // declared length would reach beyond end of file
     };
     Outcome outcome;
-    Header *header;
-    ChordTableEntry *chordTable;
+    Header header;
+    ChordTableEntry chordTable[MAX_CHORD_COUNT];
+    u32 StringLocationsTable[MAX_STRING_COUNT];
 };
 
 TwiddlerConfig parseTwiddlerConfigV5Bytes(Arena *arena, u8 *sourceBytes, u64 length, char *fileName)
@@ -166,13 +181,52 @@ TwiddlerConfig parseTwiddlerConfigV5Bytes(Arena *arena, u8 *sourceBytes, u64 len
         }
         else
         {
+            u32 stringCount = (header->mouse_left_action == 0xff) + (header->mouse_middle_action == 0xff) + (header->mouse_right_action == 0xff);
+
             ChordTableEntry *chordTable = (ChordTableEntry *)(sourceBytes + sizeof(Header));
             for (u32 chordIndex = 0;
                  chordIndex < header->chord_count;
                  ++chordIndex)
             {
                 ChordTableEntry *chord = chordTable + chordIndex;
-                printf("chord buttons=%04x code=0x%02x mod=%02x\n", chord->buttons, chord->hidCode, chord->hidModifiers);
+                printf("chord buttons=%04x code=0x%02x mod=%02x\n", chord->buttons, chord->hid.code, chord->hid.modifiers);
+                if (chord->hid.modifiers == 0xff)
+                {
+                    stringCount += 1;
+                }
+            }
+
+            if (stringCount > MAX_STRING_COUNT)
+            {
+                result = {TwiddlerConfig::Outcome::TooManyStrings};
+            }
+            else
+            {
+                u32 *stringLocationTable = (u32 *)(chordTable + header->chord_count);
+                // StringContentsTableEntry *stringContentsTable = (StringContentsTableEntry *)(stringLocationTable + stringCount);
+
+                for (u32 stringIndex = 0;
+                     stringIndex < stringCount;
+                     ++stringIndex)
+                {
+                    StringContentsTableEntry *stringEntry = (StringContentsTableEntry *)(sourceBytes + stringLocationTable[stringIndex]);
+                    if ((u8 *)stringEntry + stringEntry->length > sourceBytes + length)
+                    {
+                        result = {TwiddlerConfig::Outcome::StringHasInconsistentLength};
+                        break;
+                    }
+                    else
+                    {
+                        u32 keystrokeCount = (stringEntry->length - sizeof(StringContentsTableEntry)) / sizeof(HidPair);
+                        for (u32 keystrokeIndex = 0;
+                             keystrokeIndex < keystrokeCount;
+                             ++keystrokeIndex)
+                        {
+                            HidPair *keystroke = &stringEntry->elements[keystrokeIndex];
+                            printf("string %d, keystroke %d: code=0x%02x, mod=0x%02x\n", stringIndex, keystrokeIndex, keystroke->code, keystroke->modifiers);
+                        }
+                    }
+                }
             }
         }
     }
